5a3d66dfd9a30bc604fceab19752e2631b943085
Dorance Agudelo Rodriguez <dorance.agudelo@utp.edu.co>
2026-05-14 20:15:34 -0500

    Desarrollo de interfaz gráfica con Tkinter y punto de entrada main

---
+++ src/interfaz.py
@@ -0,0 +1,33 @@
+import tkinter as tk 
+
+from controlador import Controlador 
+
+from persistencia import guardar_log 
+
+ 
+
+class VentanaInvernadero: 
+
+    def __init__(self, root): 
+
+        self.ctrl = Controlador() 
+
+        root.title("Sistema Invernadero Virtual") 
+
+        self.label = tk.Label(root, text="Iniciando...", font=("Arial", 14)) 
+
+        self.label.pack(pady=20) 
+
+        self.actualizar() 
+
+ 
+
+    def actualizar(self): 
+
+        t, h, v, r = self.ctrl.procesar() 
+
+        guardar_log(t, h, v, r) 
+
+        self.label.config(text=f"Temp: {t}°C | Hum: {h}%\nVentilador: {v}\nRiego: {r}") 
+
+        tk.Frame().after(2000, self.actualizar) # Refrescar cada 2 segundos 
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import tkinter as tk 
+
+from interfaz import VentanaInvernadero 
+
+ 
+
+if __name__ == "__main__": 
+
+    root = tk.Tk() 
+
+    app = VentanaInvernadero(root) 
+
+    root.mainloop()
